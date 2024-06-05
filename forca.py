eb6863e78ec6984a99a471ea6dc02357bc0c89ef
camixz <128156859+camixz@users.noreply.github.com>
2024-06-05 09:06:54 -0300

    Add files via upload

--- forca.py
+++ forca.py
@@ -3,17 +3,24 @@ def jogar_forca():
     print('*********************************')
     print('Bem vindo, ao JOGO FORCA')
     print('*********************************')
+
     #Definir qual a palavra secreta
-    palavraSecreta = "bergamota"
+    palavraSecreta = "banana"
 
     enforcou = False
     acertou = False
 
     #Enquanto o jogador não se "enforcar" E não acertar a palavra, faça algo
     while(not enforcou and not acertou):
+        chute = input("Escolha uma letra!")
+        for letra in palavraSecreta:
+            if(chute == letra):
+                print(letra)
+                
         print("Escolha outra letra!")
 
 
     print("Fim de jogo!")
+
 if(__name__ == "__main__"):
     jogar_forca()
